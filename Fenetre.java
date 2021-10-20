5e43a52860735075436ee7e8137c8df7bb1671c9
jem2000-github <jeremybeaudoin.work@gmail.com>
2021-10-19 20:11:49 -0400

    test1

---
+++ Fenetre.java
@@ -0,0 +1,15 @@
+public class Fenetre {
+
+    private String frame = "carré";
+
+    public String getFrame() {
+        return frame;
+    }
+
+    public void setFrame(String frame) {
+        this.frame = frame;
+    }
+
+
+    
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Fenetre fen = new Fenetre();
+
+        System.out.println(fen.getFrame());
+
+    }
+    
+}
